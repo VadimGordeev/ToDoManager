603a5fe1d29981d3e41abd494439ca84e416eac9
VadimGordeev <vadimgordeev22@gmail.com>
2025-11-30 14:00:52 +0100

    feat: create custom cell with xib

--- ToDo Manager/TaskTypeController.swift
+++ ToDo Manager/TaskTypeController.swift
@@ -8,82 +8,60 @@
 import UIKit
 
 class TaskTypeController: UITableViewController {
-
+    
+    //    1. кортеж, описывающий тип задачи
+    typealias TypeCellDescription = (type: TaskPriority, title: String, description: String)
+    
+    //    2. коллекция доступных типов задач с их описанием
+    private var taskTypesInformation: [TypeCellDescription] = [
+        (type: .important, title: "Important", description: "This type of task is the highest priority for completion. All important tasks are displayed at the very top of the task list."),
+        (type: .normal, title: "Normal", description: "Task with normal priority.")
+    ]
+    
+    //    3. выбранный приоритет
+    var selectedType: TaskPriority = .normal
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        //        1. получение значения типа UINib, соответствующее xib-файлу кастомной ячейки
+        let cellTypeNib = UINib(nibName: "TaskTypeCell", bundle: nil)
+        //        2. регистрация кастомной ячейки в табличном представлении
+        tableView.register(cellTypeNib, forCellReuseIdentifier: "TaskTypeCell")
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 80
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return taskTypesInformation.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        //        1. получение переиспользуемой кастомной ячейки по ее идентификатору
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTypeCell", for: indexPath) as! TaskTypeCell
+        
+//        2. получаем текущий элемент, информация о котором должна быть выведена в строке
+        let typeDescription = taskTypesInformation[indexPath.row]
+        
+//        3. заполняем ячейку данными
+        cell.typeTitle.text = typeDescription.title
+        cell.typeDescription.text = typeDescription.description
+        
+//        4. если тип является выбранным, то отмечаем его галочкой
+        if selectedType == typeDescription.type {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ ToDo Manager/View/Cells/TaskTypeCell.swift
@@ -0,0 +1,26 @@
+//
+//  TaskTypeCell.swift
+//  ToDo Manager
+//
+//  Created by Vadim on 30/11/2025.
+//
+
+import UIKit
+
+class TaskTypeCell: UITableViewCell {
+    
+    @IBOutlet var typeTitle: UILabel!
+    @IBOutlet var typeDescription: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
